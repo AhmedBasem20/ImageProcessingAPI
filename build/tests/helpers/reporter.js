d9bba15e65f737b2e79a9f984339289ed1545710
Ahmed Basem <62952819+AhmedBasem20@users.noreply.github.com>
2022-03-10 18:16:38 +0200

    Update reporter.js

--- build/tests/helpers/reporter.js
+++ build/tests/helpers/reporter.js
@@ -15,6 +15,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
+//in progress
 var jasmine_spec_reporter_1 = require("jasmine-spec-reporter");
 var CustomProcessor = /** @class */ (function (_super) {
     __extends(CustomProcessor, _super);
@@ -22,14 +23,14 @@ var CustomProcessor = /** @class */ (function (_super) {
         return _super !== null && _super.apply(this, arguments) || this;
     }
     CustomProcessor.prototype.displayJasmineStarted = function (info, log) {
-        return "".concat(log);
+        return "TypeScript ".concat(log);
     };
     return CustomProcessor;
 }(jasmine_spec_reporter_1.DisplayProcessor));
 jasmine.getEnv().clearReporters();
 jasmine.getEnv().addReporter(new jasmine_spec_reporter_1.SpecReporter({
     spec: {
-        displayStacktrace: jasmine_spec_reporter_1.StacktraceOption.NONE
+        displayStacktrace: jasmine_spec_reporter_1.StacktraceOption.NONE,
     },
     customProcessors: [CustomProcessor],
 }));
