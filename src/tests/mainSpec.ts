e719d017ec7318b29ecb68edd31aee6b8c48c7b2
Ahmed Basem <62952819+AhmedBasem20@users.noreply.github.com>
2022-03-10 18:12:14 +0200

    endpoint testing

--- src/tests/mainSpec.ts
+++ src/tests/mainSpec.ts
@@ -1,1 +1,26 @@
 //in progress
+import { exit } from "process";
+// import Exist from  "../routes/api/index";
+// import resized from  "../routes/api/index";
+import supertest from "supertest";
+import app from '../main';
+
+const request = supertest(app);
+describe('Test endpoint responses', () => {
+    it('gets the api endpoint', async () => {
+        const response = await request.get('/api');
+        expect(response.status).toBe(200);
+        
+    });
+});
+
+//expect(resized).toBeTruthy();
+// async () => {
+//     const result = await Exist();
+//     expect(result).toBeTruthy();
+// }
+// () => {
+//     return Exist().then( result => {
+//         expect(result).toBeTruthy();
+//     })
+// }
